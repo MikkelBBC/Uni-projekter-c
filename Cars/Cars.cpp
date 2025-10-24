5c664a0453e2bcabbf65bee625d2e9a5f10ae4fe
MikkelBBC <mikkel.hoimark@gmail.com>
2025-10-24 18:13:41 +0100

    HJÆLP

---
+++ Cars/Cars.cpp
@@ -0,0 +1,18 @@
+#include "Cars.h"
+
+using namespace std;
+
+Cars::Cars()
+{
+    
+}
+
+Cars::~Cars()
+{
+    
+}
+
+void Cars::printout()
+{
+    cout << "Cars Rock!!" << endl;
+}
---
+++ Cars/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Cars.h"
+
+using namespace std;
+
+int main()
+{
+    Cars c;
+    cout << "Hello World!" << endl;
+    c.printout();
+}
